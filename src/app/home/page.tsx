84ca69188baad15b531c6566594d5b181c1e99f2
Raghavan <raghavan.work@outlook.com>
2025-01-03 23:41:46 +0530

    responsive login page with logos and dynamic image sizing
    - Implemented a responsive layout for the login page.
    - Added dynamic resizing for logos using Next.js Image component and Tailwind CSS.
    - Centered the main login section with a single flexbox utility class.
    - Adjusted logo sizes to scale for smaller screens.
    - Ensured proper alignment for top-left and top-right logos across screen sizes.
    - Created a GitHub login button with hover and transition effects.
    - Utilized a gradient background with smooth responsiveness.

---
+++ src/app/home/page.tsx
@@ -0,0 +1,9 @@
+export default function home() {
+    return (
+      <main>
+        <h1>home</h1>
+       
+      </main>
+    );
+  }
+  
---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+export default function NotFound() {
+    return (
+      <main>
+        <h1>Page Not Found</h1>
+        <p>Sorry, the page you’re looking for does not exist.</p>
+      </main>
+    );
+  }
+  
