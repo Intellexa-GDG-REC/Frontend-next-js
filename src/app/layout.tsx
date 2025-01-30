9ac0cdf061a288f27b4230dbee502542f1153734
sivaplaysmC <sivaram.asdf@gmail.com>
2025-01-30 21:33:06 +0530

    Hack: QuickFix: wrapp smoothscroller in a suspense to fix error
    ⨯ useSearchParams() should be wrapped in a suspense boundary at page
    "/admindashboard". Read more:
    https://nextjs.org/docs/messages/missing-suspense-with-csr-bailout    at
    o (/home/hknhmr/Frontend-next-js/.next/server/chunks/343.js:1:3580)
    at d (/home/hknhmr/Frontend-next-js/.next/server/chunks/343.js:1:20104)
    at o
    (/home/hknhmr/Frontend-next-js/.next/server/app/admindashboard/page.js:1:14195)
    at nO
    (/home/hknhmr/Frontend-next-js/node_modules/next/dist/compiled/next-server/app-page.runtime.prod.js:20:45959)
    at nI
    (/home/hknhmr/Frontend-next-js/node_modules/next/dist/compiled/next-server/app-page.runtime.prod.js:20:47734)
    at nI
    (/home/hknhmr/Frontend-next-js/node_modules/next/dist/compiled/next-server/app-page.runtime.prod.js:20:62515)
    at nL
    (/home/hknhmr/Frontend-next-js/node_modules/next/dist/compiled/next-server/app-page.runtime.prod.js:20:65533)
    at nN
    (/home/hknhmr/Frontend-next-js/node_modules/next/dist/compiled/next-server/app-page.runtime.prod.js:20:63164)
    at nB
    (/home/hknhmr/Frontend-next-js/node_modules/next/dist/compiled/next-server/app-page.runtime.prod.js:20:68946)
    at nD
    (/home/hknhmr/Frontend-next-js/node_modules/next/dist/compiled/next-server/app-page.runtime.prod.js:20:67647)Error
    occurred prerendering page "/admindashboard". Read more:
    https://nextjs.org/docs/messages/prerender-errorExport encountered an
    error on /admindashboard/page: /admindashboard, exiting the build.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { AuthProvider } from "@/components/AuthProvider";
 import "./globals.scss";
 import SmoothScroller from "@/components/Lenis";
 import Header from "@/components/Header"
+import { Suspense } from "react";
 
 
 
@@ -15,7 +16,9 @@ export default function RootLayout({
       <body
         className={`antialiased font-clash bg-[#0a0a0a] flex justify-center overflow-x-hidden`}>
         <AuthProvider>
-          <SmoothScroller />
+          <Suspense>
+            <SmoothScroller />
+          </Suspense>
           <Header />
           {children}
         </AuthProvider>
